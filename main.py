cd8fdd20312fd635ec5ce4d34423f498815f45ac
Dima <chernoskutov1989dv@mail.ru>
2022-10-25 12:36:55 +0500

    working version

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2
+
+img = cv2.imread("D:/Progs/Ram.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = cv2.CascadeClassifier("faces.xml")
+results = faces.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 3)
+for (x, y, w, h) in results:
+    cv2.rectangle(img, (x, y), (x + w, y +h ), (0, 0, 255), thickness = 3)
+cv2.imshow("Result", img)
+cv2.waitKey(0)
